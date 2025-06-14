7930dde8761319fcecd7479f1efe42ded668f4f4
Athumjs <athumjj@gmail.com>
2025-06-14 02:49:24 -0300

    uptade 1.0.3 (command help) (message of error)

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,11 +2,12 @@ export function message_help(): void {
   console.log("\x1b[1;33m");
   console.log(" " + " ".repeat(10) + "┌ Commands list ┐");
   console.log("\x1b[1;34m");
-  console.log(" →" + " help (-h) - show commands list.");
-  console.log(" →" + " version (-v) - show scriptmc version.");
-  console.log(" →" + " new (-n) - create new addon.");
-  console.log(" →" + " build (-b) - build addon.");
-  console.log(" →" + " delete (-d) - delete a addon.");
-  console.log(" →" + " path (-p) - change minecraft folder path.");
+  console.log(" →" + " --help (-h) - show commands list.");
+  console.log(" →" + " --version (-v) - show scriptmc version.");
+  console.log(" →" + " --new (-n) - create new addon.");
+  console.log(" →" + " --build (-b) - build addon.");
+  console.log(" →" + " --delete (-d) - delete a addon.");
+  console.log(" →" + " --path (-p) - change minecraft folder path.");
+  console.log(" →" + " start - start transpiler ts to js.");
   console.log("\x1b[0m");
 }
